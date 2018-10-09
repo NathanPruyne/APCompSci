832557f2d557081d593cac1d2a703e4f26508fa3
NathanPruyne <nathan.pruyne@gmail.com>
2018-10-09 09:08:54 -0500

    10/9/18, loops intro

--- Chapter 6/src/LoopsIntroduction.java
+++ Chapter 6/src/LoopsIntroduction.java
@@ -6,7 +6,26 @@ public class LoopsIntroduction {
 		for (int i = 0; i < 20; i++) {
 			System.out.println("Hello World!");
 		}
+		//example 2.0: add all #s 1-30
+		int total = 0;
+		//parts of loop: (counter initialization, condition check, counter update)
+		for (int i = 1; i <= 30; i++) {
+			total += i;
+		}
+		System.out.println(total);
 
+		//example 2.1: add all ODD #s 9-51 (inclusive). Print result.
+		int sum = 0;
+		for (int i = 9; i <= 51; i += 2) {
+			sum += i;
+		}
+		//System.out.println(i) will fail because i is local only to loop
+		System.out.println(sum);
+		
+		/* for (int i = 1; i <= 10; i--) { //about to be infinite, bad!
+			System.out.println(i);
+		} */
+		
 	}
 
 }
