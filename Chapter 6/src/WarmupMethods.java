e29902b92b3772c5eb6a5671744c4c7d563dc842
NathanPruyne <nathan.pruyne@gmail.com>
2018-10-11 08:32:19 -0500

    October 11 warmups

---
+++ Chapter 6/src/WarmupMethods.java
@@ -0,0 +1,32 @@
+
+public class WarmupMethods {
+	public static int sumUp(int low, int high) {
+		int start;
+		if (low % 3 == 0) {
+			start = low;
+		} else {
+			start = low + (3 - low % 3);
+		}
+		int sum = 0;
+		for (int i = start; i <= high; i += 3) {
+			if (i % 5 != 0) {
+				sum += i;
+			}
+		}
+		return sum;
+	}
+	
+	public static int sumDigits(int num) {
+		int numDigits = Integer.toString(num).length();
+		int sum = 0;
+		for (int i = 0; i < numDigits; i++) {
+			sum += num / Math.pow(10, i) % 10;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sumDigits(50249));
+		System.out.println(sumDigits(16));
+	}
+}
