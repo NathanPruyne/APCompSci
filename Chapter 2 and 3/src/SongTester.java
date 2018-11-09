d61034658460872ac456c32213e8046db639a34f
NathanPruyne <nathan.pruyne@gmail.com>
2018-11-08 19:50:04 -0600

    Added some songs to SongTester

--- Chapter 2 and 3/src/SongTester.java
+++ Chapter 2 and 3/src/SongTester.java
@@ -7,11 +7,57 @@ public class SongTester {
 		ArrayList<Note> notes = new ArrayList<Note>();
 		
 		
-		notes.add(new Note(2, 56, 100));
-		notes.add(new Note(2, 58, 100));
-		notes.add(new Note(1.5, 60, 100));
+		/* notes.add(new Note(2, 60, 30));
+		notes.add(new Note(2, 62, 40));
+		notes.add(new Note(2, 64, 50));
+		notes.add(new Note(2, 65, 60));
+		notes.add(new Note(2, 67, 70));
+		notes.add(new Note(2, 69, 80));
+		notes.add(new Note(2, 71, 90));
+		notes.add(new Note(2, 72, 100));
 		
-		Song mySong = new Song("Daydreams", "Eb", 2.35, notes, 150);
+		Song mySong = new Song("Scale", "C", 2.35, notes, 100); */
+		
+		int C = 60;
+		int D = 62;
+		int E = 64;
+		int F = 65;
+		int G = 67;
+		int vol = 80;
+		
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1, F, vol));
+		notes.add(new Note(1, G, vol));
+		notes.add(new Note(1, G, vol));
+		notes.add(new Note(1, F, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1, D, vol));
+		notes.add(new Note(1, C, vol));
+		notes.add(new Note(1, C, vol));
+		notes.add(new Note(1, D, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1.5, E, vol));
+		notes.add(new Note(0.5, D, vol));
+		notes.add(new Note(2, D, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1, F, vol));
+		notes.add(new Note(1, G, vol));
+		notes.add(new Note(1, G, vol));
+		notes.add(new Note(1, F, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1, D, vol));
+		notes.add(new Note(1, C, vol));
+		notes.add(new Note(1, C, vol));
+		notes.add(new Note(1, D, vol));
+		notes.add(new Note(1, E, vol));
+		notes.add(new Note(1.5, D, vol));
+		notes.add(new Note(0.5, C, vol));
+		notes.add(new Note(2, C, vol));
+		
+		Song mySong = new Song("Ode to Joy", "C", 8, notes, 120);
+		
 		System.out.println(mySong);
 		mySong.playSong();
 		
