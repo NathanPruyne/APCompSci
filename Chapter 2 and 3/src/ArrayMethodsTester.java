16a9c59c1ca1d78dd35348a4cc46631d5cf332fd
NathanPruyne <nathan.pruyne@gmail.com>
2018-12-04 08:53:17 -0600

    Array method testers

--- Chapter 2 and 3/src/ArrayMethodsTester.java
+++ Chapter 2 and 3/src/ArrayMethodsTester.java
@@ -1,23 +1,27 @@
-import java.util.Arrays;
-
 
 public class ArrayMethodsTester {
 
-	public static void main(String[] args) {
-		int[] values = {5, 5, 2, 3, 8, 1, 1, 6, 6, 7, 5};
-		System.out.println(ArrayMethods.display(values));
-		System.out.println(ArrayMethods.sum(values));
-		System.out.println(ArrayMethods.average(values));
-		System.out.println(Arrays.toString(ArrayMethods.loadArray(5, 10)));
-		System.out.println(ArrayMethods.max(values));
-		System.out.println(ArrayMethods.max(values, 7, 9));
-		System.out.println(ArrayMethods.maxIndex(values));
-		System.out.println(ArrayMethods.min(values));
-		System.out.println(ArrayMethods.minIndex(values));
-		System.out.println(ArrayMethods.occurrences(values, 5));
-		System.out.println(ArrayMethods.occurrences(values, 0, 6, 5));
-		System.out.println(ArrayMethods.repeats(values));
-		System.out.println(ArrayMethods.largestGap(values));
+	public static void main(String[] args) 
+	{
+		int[] randomArray = ArrayMethods.loadArray(6, 50);
+		System.out.println(ArrayMethods.display(randomArray));
+		
+		int[] values = {5, 3, 8, 23, 7, 3, 9, 3, 10, 3, 3, 5, 4};
+		System.out.println(ArrayMethods.sum(values) == 86);
+		System.out.println(ArrayMethods.average(values) == 86.0/values.length);
+		System.out.println(ArrayMethods.max(values) == 23);
+		System.out.println(ArrayMethods.max(values, 4, 7) == 9);
+		System.out.println(ArrayMethods.maxIndex(values) == 3);
+		System.out.println(ArrayMethods.min(values) == 3);
+		System.out.println(ArrayMethods.minIndex(values) == 1);
+		System.out.println(ArrayMethods.occurrences(values, 3) == 5);
+		System.out.println(ArrayMethods.occurrences(values, 4, 9, 3) == 3);
+		System.out.println(ArrayMethods.repeats(values) == 1);
+		System.out.println(ArrayMethods.largestGap(values)==3);
+		
+		
+		
+		
 	}
-	
+
 }
