441b474c42aa15f20061cebad720c083032d6430
NathanPruyne <nathan.pruyne@gmail.com>
2019-01-09 09:00:32 -0600

    Linear and Binary Searches

--- SearchAndSortArrays/src/SearchAndSort.java
+++ SearchAndSortArrays/src/SearchAndSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 
 public class SearchAndSort
 {
@@ -5,20 +7,56 @@ public class SearchAndSort
 	//postcondition: return the index of the target, or -1 if target is not in the array
 	public static int linearSearch(int[] values, int target)
 	{
-		
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] == target) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	//overloaded for String arrays
 	public static int linearSearch(String[] values, String target)
 	{
-		
+		for (int i = 0; i < values.length; i++) {
+			if (values[i].equals(target)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	//precondition: values is already sorted
 	//postcondition: return the index of the target, or -1 if target is not in the array
 	public static int binarySearch(int[] values, int target)
 	{
-		
+		int first = 0;
+		int last = values.length - 1;
+		int middle;
+		while (first <= last) {
+			middle = (first + last) / 2;
+			if (values[middle] == target) {
+				return middle;
+			} else if (values[middle] > target) {
+				first = middle + 1;
+			} else {
+				last = middle - 1;
+			}
+		}
+		return -1;
+	}
+	
+	public static int binarySearchRecursive(int[] values, int target) {
+		int middle = (values.length - 1) / 2;
+		if (values[middle] == target) {
+			return middle;
+		} else if (values.length == 1) {
+			return -1;
+		} else if (values[middle] < target) {
+			return binarySearchRecursive(Arrays.copyOfRange(values, middle + 1, values.length - 1), target);
+		} else {
+			return binarySearchRecursive(Arrays.copyOfRange(values, 0, middle - 1), target);
+		}
 	}
 	
 	//String version
