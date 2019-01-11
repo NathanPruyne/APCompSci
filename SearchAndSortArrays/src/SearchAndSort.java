4e541e95692200d3b7784b60cfeb45f4269db75d
NathanPruyne <nathan.pruyne@gmail.com>
2019-01-11 08:54:36 -0600

    Search and sort completed

--- SearchAndSortArrays/src/SearchAndSort.java
+++ SearchAndSortArrays/src/SearchAndSort.java
@@ -37,7 +37,7 @@ public class SearchAndSort
 			middle = (first + last) / 2;
 			if (values[middle] == target) {
 				return middle;
-			} else if (values[middle] > target) {
+			} else if (values[middle] < target) {
 				first = middle + 1;
 			} else {
 				last = middle - 1;
@@ -70,7 +70,7 @@ public class SearchAndSort
 			int comparison = values[middle].compareTo(target);
 			if (comparison == 0) {
 				return middle;
-			} else if (comparison > 0) {
+			} else if (comparison < 0) {
 				first = middle + 1;
 			} else {
 				last = middle - 1;
@@ -121,33 +121,88 @@ public class SearchAndSort
 	//String version
 	public static String[] bubbleSort(String[] values)
 	{
-		
+		boolean swapped = true;
+		String temp;
+		while (swapped) {
+			swapped = false;
+			for (int i = 0; i < values.length - 1; i++) {
+				if (values[i].compareTo(values[i + 1]) > 0) {
+					temp = values[i];
+					values[i] = values[i + 1];
+					values[i + 1] = temp;
+					swapped = true;
+				}
+			}
+		}
+		return values;
 	}
 	
 	//precondition: values is a non-empty array of integers
 	//postcondition: returns a new array that is the sorted version of the input parameter
 	public static int[] selectionSort(int[] values)
 	{
-		
+		int smallestIndex;
+		for (int i = 0; i < values.length; i++) {
+			smallestIndex = i;
+			for (int j = i; j < values.length; j++) {
+				if (values[j] < values[smallestIndex]) {
+					smallestIndex = j;
+				}
+			}
+			int temp = values[i];
+			values[i] = values[smallestIndex];
+			values[smallestIndex] = temp;
+		}
+		return values;
 	}
 	
 	//String version
 	public static String[] selectionSort(String[] values)
 	{
-		
+		int smallestIndex;
+		for (int i = 0; i < values.length; i++) {
+			smallestIndex = i;
+			for (int j = i; j < values.length; j++) {
+				if (values[j].compareTo(values[smallestIndex]) < 0) {
+					smallestIndex = j;
+				}
+			}
+			String temp = values[i];
+			values[i] = values[smallestIndex];
+			values[smallestIndex] = temp;
+		}
+		return values;
 	}
 	
 	//precondition: values is a non-empty array of integers
 	//postcondition: returns a new array that is the sorted version of the input parameter
 	public static int[] insertionSort(int[] values)
 	{
-	
+		for (int i = 1; i < values.length; i++) {
+			int j = i;
+			int temp = values[i];
+			while (j > 0 && temp < values[j - 1]) {
+				values[j] = values[j - 1];
+				j--;
+			}
+			values[j] = temp;
+		}
+		return values;
 	}
 	
 	//String version
 	public static String[] insertionSort(String[] values)
 	{
-		
+		for (int i = 1; i < values.length; i++) {
+			int j = i;
+			String temp = values[i];
+			while (j > 0 && temp.compareTo(values[j - 1]) < 0) {
+				values[j] = values[j - 1];
+				j--;
+			}
+			values[j] = temp;
+		}
+		return values;
 	}
 	
 	public static boolean isSorted(int[] values) {
--- SearchAndSortArrays/src/SearchAndSortTest.java
+++ SearchAndSortArrays/src/SearchAndSortTest.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 public class SearchAndSortTest {
 	
 	public static void main(String[] args) {
+		int[] values = {-3, -1, 4, 6, 11, 16, 22, 42, 50, 51, 61, 75, 80, 89, 203, 514, 719, 1012};
+		System.out.print((SearchAndSort.binarySearch(values, 42)));
+		
 		int[] array1 = {3, 5, 7, 6, -2, 0, 9, 8};
-		System.out.println(SearchAndSort.isSorted((SearchAndSort.slightlyFasterBubble(array1))));
+		System.out.println(SearchAndSortTester.display((SearchAndSort.insertionSort(array1))));
 	}
 	
 }
