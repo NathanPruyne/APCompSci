2e4e194d73107d845be14c974b92b3732cebff80
NathanPruyne <nathan.pruyne@gmail.com>
2019-02-26 09:01:44 -0600

    Recursive string reversal

--- Recursion/src/RecursiveMethods.java
+++ Recursion/src/RecursiveMethods.java
@@ -14,9 +14,16 @@ public class RecursiveMethods {
 		return fib(n - 2) + fib(n - 1);
 	}
 	
+	public static String reverse(String s) {
+		if (s.length() < 2) {
+			return s;
+		}
+		return s.substring(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println(fib(11));
+		System.out.println(reverse("reversal"));
 		
 	}
 
